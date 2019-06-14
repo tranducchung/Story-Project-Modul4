b768cffddcc3b317cb0ad3dd5a9475281934c818
chung <tranducchung1st@gmail.com>
2019-06-14 08:39:52 +0700

    Chung commit first

---
+++ src/main/java/com/codegym/cms/StoryProjectApplication.java
@@ -0,0 +1,13 @@
+package com.codegym.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoryProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoryProjectApplication.class, args);
+	}
+
+}
